81964349fcbe6f5f9a3f087f62a0056feedca920
WellingtonEugenio <wellingtoneugenio@gmail.com>
2024-05-17 10:07:16 -0300

    iniciando projeto shop-api

---
+++ src/main/com/santana/ShopApi.java
@@ -0,0 +1,17 @@
+package com.santana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApi {
+
+	public static void main(String[] args) {
+		// Iniciar springboot
+		
+		System.out.println("------------");
+		SpringApplication.run(ShopApi.class, args);
+
+	}
+
+}
